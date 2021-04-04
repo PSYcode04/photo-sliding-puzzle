41f9f0651b60b0e5191a9bc8b45049c04bc2812d
PSYcode04 <psy.code04@gmail.com>
2021-04-05 02:03:39 +0900

    constraintLayout -> LinearLayout, create alertDialog

--- app/src/main/java/com/example/photoslidingpuzzle/MainActivity.java
+++ app/src/main/java/com/example/photoslidingpuzzle/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.photoslidingpuzzle;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,11 +39,28 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         
         if(item.getItemId() == R.id.action_info) {
+            showInfo();
             Toast.makeText(getApplicationContext(), "버튼 클릭", Toast.LENGTH_SHORT).show();
             return true;
         }
         else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void showInfo(){
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+
+        alertBuilder.setTitle("** 게임 설명 **");
+        alertBuilder.setMessage("이미지를 클릭하면 원하는 이미지로 바꿀 수 있습니다.");
+        alertBuilder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Pressed OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog alertDialog = alertBuilder.create();
+        alertDialog.show();
     }
 }
