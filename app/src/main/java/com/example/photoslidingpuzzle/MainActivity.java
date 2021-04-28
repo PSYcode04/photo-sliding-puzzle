b0280b09813c3edc692a5275a23dbf845e2539b1
PSYcode04 <psy.code04@gmail.com>
2021-04-28 15:25:19 +0900

    이미지 선택시, 갤러리에서 사진 선택 기능 추가

--- app/src/main/java/com/example/photoslidingpuzzle/MainActivity.java
+++ app/src/main/java/com/example/photoslidingpuzzle/MainActivity.java
@@ -1,34 +1,143 @@
 package com.example.photoslidingpuzzle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-//    Toolbar toolbar;
+    private static final int PERMISSION_FILE = 23;
+    private static final int ACCESS_FILE = 43;
 
     ActionBar actionBar;
+    ImageView imageView;
+    Bitmap selectImage;
+    Button select3by3;
+    Button select4by4;
+    Button shuffle;
 
+    ImageView testImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkSelfPermission();  // Check Permission
 
         actionBar = getSupportActionBar();
+        imageView = (ImageView) findViewById(R.id.originalImage);
+        select3by3 = (Button) findViewById(R.id.button1);
+        select4by4 = (Button) findViewById(R.id.button2);
 
+        testImage = (ImageView) findViewById(R.id.imageView);
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_FILE);
+                } else {
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_PICK);
+                    startActivityForResult(Intent.createChooser(intent, "select"), ACCESS_FILE);
+                }
+            }
+        });
+    }
+    @Override
+    protected  void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == ACCESS_FILE && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            Uri FILE_URI = data.getData();
+
+            CropImage.activity(FILE_URI)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                imageView.setImageURI(resultUri);
+                selectImage = null;
+                try {
+                    selectImage = MediaStore.Images.Media.getBitmap(getContentResolver(),result.getUri());
+                    testImage.setImageBitmap(selectImage);
+                    Toast.makeText(this, "Select!", Toast.LENGTH_SHORT).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 
+    /////권한 요청///
+    public void checkSelfPermission() {
+        String temp = "";
+
+        //파일 읽기 권한 확인
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+        }
+
+        //파일 쓰기 권한 확인
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+        }
+
+        if(TextUtils.isEmpty(temp) == false) {
+            // 권한 요청
+            ActivityCompat.requestPermissions(this, temp.trim().split(" "),1);
+        } else {
+            // 모두 허용 상태
+            Toast.makeText(this, "권한을 모두 허용", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    ///////TOOLBAR//////////
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -40,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
         
         if(item.getItemId() == R.id.action_info) {
             showInfo();
-            Toast.makeText(getApplicationContext(), "버튼 클릭", Toast.LENGTH_SHORT).show();
+            // Toast.makeText(getApplicationContext(), "버튼 클릭", Toast.LENGTH_SHORT).show();
             return true;
         }
         else {
@@ -52,15 +161,17 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
 
         alertBuilder.setTitle("** 게임 설명 **");
-        alertBuilder.setMessage("이미지를 클릭하면 원하는 이미지로 바꿀 수 있습니다.");
+        alertBuilder.setMessage("이미지를 클릭하면 원하는 이미지로 바꿀 수 있습니다.\n\nSHUFFLE 버튼을 클릭하면 게임을 시작할 수 있습니다. ");
         alertBuilder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(getApplicationContext(), "Pressed OK", Toast.LENGTH_SHORT).show();
             }
         });
 
         AlertDialog alertDialog = alertBuilder.create();
         alertDialog.show();
     }
+    /////////////////////////////////////////
+
 }
+
