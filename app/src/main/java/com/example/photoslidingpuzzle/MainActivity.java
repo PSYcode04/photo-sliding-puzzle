057dc54a6fbd04b778af8f327c6a4451c48c43c6
PSYcode04 <psy.code04@gmail.com>
2021-04-28 19:30:15 +0900

    이미지 선택 후 resize, 슬라이딩 퍼즐 구현

--- app/src/main/java/com/example/photoslidingpuzzle/MainActivity.java
+++ app/src/main/java/com/example/photoslidingpuzzle/MainActivity.java
@@ -14,14 +14,19 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -32,11 +37,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSION_FILE = 23;
     private static final int ACCESS_FILE = 43;
+
+    // 변수
+    public int num = 3; // 퍼즐 초기값은 3*3
+    protected Bitmap[] oriPuzzlePiece; // 이미지 비트맵 배열
+    protected PuzzlePiece[] puzzlePiece; // 퍼즐 정답 형태 배열
+    protected PuzzlePiece[] shufflePiece; // shuffle했을 때 배열
+    private int[] puzzleOrder;
+    int[][] board; // 3*3, 4*4 형태 위치를 찾기 위한 보드판
+    int loc = 0;
+    int x = 0, y = 0; // 보드판에서 좌표
+    private boolean answer = false; // 정답 체크
+    private boolean click = false; // shuffle버튼 눌렀는지 체크
+
+    int width;  // 화면 가로 크기 구하기
+
 
     ActionBar actionBar;
     ImageView imageView;
@@ -44,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
     Button select3by3;
     Button select4by4;
     Button shuffle;
+    GridView gridView;
 
-    ImageView testImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,14 +75,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         checkSelfPermission();  // Check Permission
 
+        // 화면 크기 구하기
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getRealSize(size); // or getSize(size)
+        width = size.x - 70; // 디스플레이 가로 크기에서 50(pixel) 빼기
+
         actionBar = getSupportActionBar();
         imageView = (ImageView) findViewById(R.id.originalImage);
         select3by3 = (Button) findViewById(R.id.button1);
         select4by4 = (Button) findViewById(R.id.button2);
+        shuffle = (Button) findViewById(R.id.shuffle);
+        gridView = (GridView) findViewById(R.id.GridView);
 
-        testImage = (ImageView) findViewById(R.id.imageView);
+        // 기본 이미지 보드 설정
+        selectImage = BitmapFactory.decodeResource(getResources(), R.drawable.image);
+        resizeImage();
+        setImage(3);
 
-
+        // 이미지 선택
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,10 +105,138 @@ public class MainActivity extends AppCompatActivity {
                     intent.setType("image/*");
                     intent.setAction(Intent.ACTION_PICK);
                     startActivityForResult(Intent.createChooser(intent, "select"), ACCESS_FILE);
+
+                }
+            }
+        });
+
+        // 버튼 클릭하면 나눌 숫자 변경
+        // 3*3
+        select3by3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                setImage(3);
+            }
+        });
+
+        // 4*4
+        select4by4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                setImage(4);
+            }
+        });
+
+        // SHUFFLE 버튼
+        shuffle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                click = true; // shuffle을 눌렀음을 표시
+                answer = false;
+
+                PuzzleAdapter adapter = new PuzzleAdapter(getApplicationContext(), selectImage, num, puzzlePiece);
+                Collections.shuffle(Arrays.asList(adapter.newPuzzlePiece));
+                gridView.setAdapter(adapter);
+                gridView.setNumColumns(num);
+
+                // shuffle 눌렀을 때의 퍼즐 순서 저장
+                for(int i = 0; i < adapter.newPuzzlePiece.length; i++) {
+                    shufflePiece[i] = new PuzzlePiece(adapter.newPuzzlePiece[i].getImagePiece(), adapter.newPuzzlePiece[i].getIndex());
+                }
+            }
+        });
+
+        // 퍼즐 클릭했을 때 동작
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            int clickLocation; // 클릭한 퍼즐 위치
+            int blank; // 빈칸 위치
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                clickLocation = position;
+                blank = shufflePiece.length - 1;
+
+                // shuffle버튼을 누르고, 정답이 아닐 경우에만 클릭 가능
+                if(click && !answer) {
+                    // 클릭한 퍼즐 좌표 찾기
+                    for(int h = 0; h < num; h++) {
+                        for(int w = 0; w < num; w++){
+                            if(board[h][w] == clickLocation) {
+                                y = h;
+                                x = w;
+                                break;
+                            }
+                        }
+                    }
+
+                    // 퍼즐 초기값은 3*3
+                    if(y - 1 >= 0 && shufflePiece[board[y-1][x]].getIndex() == blank){
+                        swapPiece(clickLocation, board[y-1][x]);
+                        checkAnswer();
+                    }
+                    // 아래쪽이 빈칸인 경우
+                    else if(y + 1 < num  && shufflePiece[board[y+1][x]].getIndex() == blank) {
+                        swapPiece(clickLocation, board[y+1][x]);
+                        checkAnswer();
+                    }
+                    // 오른쪽이 빈칸인 경우
+                    else if(x + 1 < num && shufflePiece[board[y][x+1]].getIndex() == blank){
+                        swapPiece(clickLocation, board[y][x+1]);
+                        checkAnswer();
+                    }
+                    // 왼쪽이 빈칸인 경우
+                    else if(x - 1 >= 0 && shufflePiece[board[y][x-1]].getIndex() == blank){
+                        swapPiece(clickLocation, board[y][x-1]);
+                        checkAnswer();
+                    }
+                    // 주변에 빈칸이 없는 공간을 클릭했을 경우는 동작 안함
+
+                    // 클릭한 후, 이동한 퍼즐을 화면에 표시
+                    PuzzleAdapter adapter = new PuzzleAdapter(getApplicationContext(), selectImage, num, shufflePiece);
+                    gridView.setAdapter(adapter);
+                    gridView.setNumColumns(num);
+
+                    // 정답이라면 Toast메시지 출력후, 더 이상 클릭 불가!
+                    if(answer)
+                        Toast.makeText(getApplicationContext(), "FINISH!", Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
+
+    // 이미지 리사이즈
+    protected void resizeImage(){
+        Bitmap resized = null;
+
+        resized = Bitmap.createScaledBitmap(selectImage, width, width, true);
+        selectImage = resized;
+    }
+
+    // 보드판 update
+    protected void setImage(int puzzleNum){
+        num = puzzleNum;
+        loc = 0;
+        click = false;
+        cutImage(selectImage);
+        answer = false;
+        puzzlePiece = new PuzzlePiece[num*num];
+        shufflePiece = new PuzzlePiece[num*num];
+
+        for(int i = 0; i < num*num; i++) {
+            puzzlePiece[i] = new PuzzlePiece(oriPuzzlePiece[i], i);
+        }
+
+        board = new int[num][num];
+        for(int i = 0; i < num; i++) {
+            for(int j = 0; j < num; j++, loc++)
+                board[i][j] = loc;
+        }
+
+        PuzzleAdapter adapter = new PuzzleAdapter(getApplicationContext(), selectImage, num, puzzlePiece);
+        gridView.setAdapter(adapter);
+        gridView.setNumColumns(num);
+    }
+
     @Override
     protected  void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -96,7 +257,8 @@ public class MainActivity extends AppCompatActivity {
                 selectImage = null;
                 try {
                     selectImage = MediaStore.Images.Media.getBitmap(getContentResolver(),result.getUri());
-                    testImage.setImageBitmap(selectImage);
+                    resizeImage();
+                    setImage(3);
                     Toast.makeText(this, "Select!", Toast.LENGTH_SHORT).show();
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
@@ -108,6 +270,46 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+
+    /////이미지, 퍼즐 설정/////////
+    // 이미지 비트맵 분할
+    public void cutImage(Bitmap image) {
+        int x, y;
+        int i = 0;
+        oriPuzzlePiece = new Bitmap[num * num];
+
+        for (y = 0; y <= (image.getHeight() - image.getHeight() / num); y += image.getHeight() / num) {
+            for (x = 0; x <= (image.getWidth() - image.getWidth() / num); x += image.getWidth() / num, i++) {
+                if(i == num*num-1)
+                    break;
+                oriPuzzlePiece[i] = Bitmap.createBitmap(image, x, y, image.getWidth()/ num, image.getHeight() / num);
+            }
+        }
+    }
+
+    // 정답인지 체크
+    private void checkAnswer() {
+
+        for(int i = 0; i < num*num; i++) {
+            if(shufflePiece[i].getIndex() == i)
+                continue;
+            else {
+                answer = false;
+                return;
+            }
+        }
+        answer = true;
+    }
+
+    // 퍼즐 위치 swap
+    public void swapPiece(int loc1, int loc2){
+        PuzzlePiece tmp1 = shufflePiece[loc1]; // 클릭한 위치
+        PuzzlePiece tmp2 = shufflePiece[loc2]; // 변경할 위치?
+
+        shufflePiece[loc1] = tmp2;
+        shufflePiece[loc2] = tmp1;
     }
 
 
